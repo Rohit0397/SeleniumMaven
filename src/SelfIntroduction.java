696c3e54e3e4a7306a941bbb848d6541bc1f8576
Rohit0397 <rohit030897@gmail.com>
2024-04-16 12:09:30 +0530

    Craeted master branch

---
+++ src/SelfIntroduction.java
@@ -0,0 +1,20 @@
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "/home/appinventiv/Downloads/chromedriver-linux64/chromedriver");
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com");
+		
+		Thread.sleep(4000);
+		driver.manage().window().maximize();
+	 
+	}
+
+}
